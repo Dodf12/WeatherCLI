9cb0d4609abe2b6ebde866b90ba1042779e48b48
Dodf12 <apala2@scu.edu>
2025-12-25 16:18:27 -0800

    fix release thing

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module weatherCLI
+module WeatherCLI
 
 go 1.22.4
 
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "weatherCLI/cmd"
+import "WeatherCLI/cmd"
 
 func main() {
 	cmd.Execute()
